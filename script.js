be1583775ed5651d74ed6f347bba7031f928f432
Aasthayuli <aasthayuli2000@gmail.com>
2024-07-10 23:45:49 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+let menu_icon_box = document.querySelector(".menu-icon-box");
+let box = document.querySelector(".box");
+
+menu_icon_box.onclick = function () {
+  menu_icon_box.classList.toggle("active");
+  box.classList.toggle("active");
+};
+
+document.querySelectorAll(".bold-span i").forEach((icon) => {
+  icon.addEventListener("mouseover", function () {
+    icon.closest(".pr").style.opacity = "0";
+  });
+  icon.addEventListener("mouseout", function () {
+    icon.closest(".pr").style.opacity = "1";
+  });
+});
+
+let btn = document.querySelector(".btn");
+
+function scrollToSection(id) {
+  document.querySelector(id).scrollIntoView({ behavior: "smooth" });
+}
+btn.addEventListener("click", function () {
+  btn.onclick = scrollToSection("#contact-id");
+});
